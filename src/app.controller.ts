9163217fab7ad366d53e1786a5ce2b3741c3709e
dev-lhk <hsb208885@gmail.com>
2025-03-04 02:39:26 +0900

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,8 +6,8 @@ export class AppController {
   @Get()
   getHello(): string {
     return `
-    안녕하세요! NestJS HWPML 수식 변환 시스템에 오신 것을 환영합니다.\n
-    Swagger를 통해 API 명세를 확인할 수 있습니다.\n
+    안녕하세요! NestJS HWPML 수식 변환 시스템에 오신 것을 환영합니다.
+    Swagger를 통해 API 명세를 확인할 수 있습니다.
     브라우저에서 다음 URL로 접근하세요. -> 
     http://localhost:3000/api
     `;
